1cdf328cae9a799884b408fb9cd6b840ccb54160
Tira Sundara <sundaralinus@gmail.com>
2025-03-06 14:37:24 +0700

    Response multiple PINGs

--- app/main.go
+++ app/main.go
@@ -12,19 +12,39 @@ var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	fmt.Println("Starting TCP server on port 6379")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+	fmt.Println("Received", n, "bytes")
+	fmt.Println("Received: ", string(buf[:n]))
+	n, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		return
+	}
+	fmt.Printf("Sent %d bytes\n", 6)
+	fmt.Printf("Sent: +PONG\r\n")
 }
